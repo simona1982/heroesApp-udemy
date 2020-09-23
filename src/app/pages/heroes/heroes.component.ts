1a33267fda7d98fa602ea830d076be598381c6e8
Andres Velasquez Delgado <monkey.velasquez.1982@gmail.com>
2020-09-23 10:38:26 -0500

    fin del CRUD

---
+++ src/app/pages/heroes/heroes.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroeModel } from 'src/app/models/heroe.model';
+import { HeroesService } from 'src/app/services/heroes.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+export class HeroesComponent implements OnInit {
+  heroes: HeroeModel[] = [];
+  cargando: boolean = false;
+
+  constructor(private heroesService: HeroesService) {}
+
+  ngOnInit(): void {
+    this.cargando = true;
+    this.heroesService.getHeroes().subscribe((resp) => {
+      console.log(resp);
+      this.heroes = resp;
+      this.cargando = false;
+    });
+  }
+
+  borrarHeroe(heroe: HeroeModel, i: number) {
+    Swal.fire({
+      title: '¿Está seguro?',
+      text: `Está seguro que desea borrar a ${heroe.nombre}`,
+      icon: 'question',
+      showConfirmButton: true,
+      showCancelButton: true,
+    }).then((resp) => {
+      if (resp.value) {
+        this.heroes.splice(i, 1);
+        this.heroesService.borrarHeroe(heroe.id).subscribe((resp) => {});
+      }
+    });
+  }
+}
